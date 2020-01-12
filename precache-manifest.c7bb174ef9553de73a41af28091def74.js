dd9d506c6f6070968de1d8249753ad35df93b909
Linn Bergström <linnber@msn.com>
2020-01-12 18:44:22 +0100

    Updates

---
+++ precache-manifest.c7bb174ef9553de73a41af28091def74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600751f366f31e1751ca18d7d2cabe6b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "1e716e4e6f7dc867eed9",
+    "url": "/react-todo/static/css/main.da5cf7aa.chunk.css"
+  },
+  {
+    "revision": "bdbbf3a42166ab06d3c2",
+    "url": "/react-todo/static/js/2.4f03a06a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo/static/js/2.4f03a06a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e716e4e6f7dc867eed9",
+    "url": "/react-todo/static/js/main.1baecfe2.chunk.js"
+  },
+  {
+    "revision": "4d6de7b78de98614c129",
+    "url": "/react-todo/static/js/runtime-main.01144c30.js"
+  }
+]);
